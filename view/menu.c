770d05ca070cfeba1bb20eb29f5d3c2d127835da
GabrielSantana013 <gabrielsantanadias133@gmail.com>
2025-04-02 14:39:53 -0300

    wip: first window completed with logo

--- view/menu.c
+++ view/menu.c
@@ -2,109 +2,44 @@
 #include <string.h>
 #include <stdlib.h>
 #include "view.h"
+#include "logo.h"
 
-#define WIDTH 40
-#define HEIGHT 20
+#define WIDTH 60
+#define HEIGHT 30
 #define MENU_COLOR 1
 #define MENU_ITEM_COLOR 2
-#define CURSOR_COLOR 3
 
-
-
-void menu(char (*ptrBufferScreen)[WIDTH]){
-
+void menu(){
     initscr();
     cbreak(); 
     noecho();
-    nodelay(stdscr, FALSE);
     keypad(stdscr, TRUE);
     curs_set(0);
     start_color();
+    
     init_pair(MENU_COLOR, COLOR_CYAN, COLOR_BLACK);
     init_pair(MENU_ITEM_COLOR, COLOR_WHITE, COLOR_BLACK);
 
-    int choice = 0;
+    refresh();
 
-    char *menu_items[] = {
-        "Start Game",
-        "Scoreboard",
-        "Instructions",
-        "Exit"
-    };
+    // Cria uma nova janela para o menu
+    WINDOW *menuWin = newwin(HEIGHT, WIDTH, 0, 0);
+    box(menuWin, 0, 0); 
+    wrefresh(menuWin);
+    refresh();
+    
+    // Posicionamento do logo
+    int start_x = 12;  
+    int start_y = 1;   
 
-    char *cursor = "-->";
-
-
-    int num_items = sizeof(menu_items) / sizeof(menu_items[0]);
-
-
-    //printar as bordas
-    for(int i = 0; i < HEIGHT; i++){
-        for(int j = 0; j < WIDTH; j++){
-            if(i == 0 || i == HEIGHT - 1 || j==0 || j == WIDTH -1){
-                if(i == 0 && j == 0){
-                    ptrBufferScreen[i][j] = TOP_LEFT_WALL;
-                } else if(i == 0 && j == WIDTH - 1){
-                    ptrBufferScreen[i][j] = TOP_RIGHT_WALL;
-                } else if(i == HEIGHT - 1 && j == 0){
-                    ptrBufferScreen[i][j] = BOTTOM_LEFT_WALL;
-                } else if(i == HEIGHT - 1 && j == WIDTH - 1){
-                    ptrBufferScreen[i][j] = BOTTOM_RIGHT_WALL;
-                } else if(i == 0 || i == HEIGHT - 1){
-                    ptrBufferScreen[i][j] = HORIZONTAL_WALL;
-                } else {
-                    ptrBufferScreen[i][j] = VERTICAL_WALL;
-                }
-                
-            } else {
-                mvaddch(i,j,' ');
-            }
-        }
-
+    for (int i = 0; i < 7; i++) {
+        mvprintw(start_y + i, start_x, "%s", menuLogo[i]);
     }
 
-/*     while(1){
-        clear();
-        for(int i = 0; i < num_items; i++){
-            if(i == choice){
-                attron(COLOR_PAIR(MENU_ITEM_COLOR));
-            } else {
-                attroff(COLOR_PAIR(MENU_ITEM_COLOR));
-            }
-            mvprintw(HEIGHT/2 + i, (WIDTH - strlen(menu_items[i])) / 2, "%s", menu_items[i]);
-        }
-        refresh();
+    
+    
+    wrefresh(menuWin);
+    getch();    
 
-        int ch = getch();
-        switch(ch){
-            case KEY_UP:
-                choice--;
-                if(choice < 0) choice = num_items - 1;
-                break;
-            case KEY_DOWN:
-                choice++;
-                if(choice >= num_items) choice = 0;
-                break;
-            case '\n':
-                if(choice == 0){
-                    // Start game
-                    return;
-                } else if(choice == 1){
-                    // Show instructions
-                    mvprintw(HEIGHT/2 + num_items + 1, (WIDTH - 20) / 2, "Instructions go here...");
-                    refresh();
-                    getch();
-                } else if(choice == 2){
-                    // Exit
-                    endwin();
-                    exit(0);
-                }
-                break;
-        }
-
-
-    }
- */
-    endwin();
-    // Cleanup and exit
+    endwin(); // Encerra ncurses
 }
--- view/view.h
+++ view/view.h
@@ -3,17 +3,7 @@
 #ifndef VIEWS_H
 #define VIEWS_H
 
-#define VERTICAL_WALL '|'
-#define HORIZONTAL_WALL '-'
-#define BOTTOM_LEFT_WALL '\\'
-#define BOTTOM_RIGHT_WALL '/'
-#define TOP_LEFT_WALL '/'
-#define TOP_RIGHT_WALL '\\'
-#define WIDTH 40
-#define HEIGHT 20
-
-
-void menu(char (*ptrBufferScreen)[WIDTH]);
+void menu();
 void gameStart(Snake *ptrSnake, Food *ptrFood, char (*ptrBufferScreen)[WIDTH]);
 void drawScreen(Snake *ptrSnake, Food *ptrFood, char (*ptrBufferScreen)[WIDTH], int *score);
 
